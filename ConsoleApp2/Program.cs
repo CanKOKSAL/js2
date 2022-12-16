181faf404745b109cbc5056b3e87be306a901d2c
Koksal Jan <JaniK@code.local>
2022-12-16 21:49:58 +0400

    ders

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp2;
+
+Console.WriteLine("Plase write your Name and Surname");
+string name = Console.ReadLine();
+string Surname = Console.ReadLine();
+
+Console.WriteLine("And your score");
+int score = int.Parse(Console.ReadLine());
+
+
+Student student = new Student(name, Surname, score);
+
+student.GetFullInfo();
+student.CanTakeExam();
+
